1e2231468d5a1d1bb9d817fbc62d46ab01b3654d
Aman Agarwal <amanagarwal8982@gmail.com>
2019-01-27 02:35:29 +0530

    second commit to add src files

---
+++ src/main/java/com/healthKart/HealthKartSpringVersionApplication.java
@@ -0,0 +1,15 @@
+package com.healthKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthKartSpringVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthKartSpringVersionApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
+
